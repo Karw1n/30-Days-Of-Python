160e9b63077ba613dff2e404b33d180a05393d6a
Karw1n <akbbalfour@gmail.com>
2025-07-22 19:44:17 +0100

    Day 19 Exercise 5 completed.

--- 19_Day_Exercise.py
+++ 19_Day_Exercise.py
@@ -83,3 +83,30 @@ with open('file_handling_data/email_exchanges_big.txt') as f:
       if len(parts) > 1:
         emails.append(parts[1]) 
         
+# 5. Find the most common words in the English language. Call the name of your function find_most_common_words, it will take two parameters - a string or a file and a positive integer, indicating the number of words. Your function will return an array of tuples in descending order. Check the output
+
+obama_speech = 'file_handling_data\obama_speech.txt'
+michelle_obama_speech = 'file_handling_data\michelle_obama_speech.txt'
+donald_speech = 'file_handling_data\donald_speect.txt'
+melina_trump_speech = 'file_handling_data\melina_trump_speech.txt'
+
+def find_most_common_words(filename, number):
+  words = {}
+  with open(filename) as f:
+    for line in f:
+      for word in line.split():
+        if word in words.keys() and word.isalpha():
+          words[word] = words[word] + 1
+        else:
+          words[word] = 1
+  top_words = sorted(words, key=words.get, reverse=True)[:number]
+  tup_list = []
+  for word in top_words:
+    tup_list.append((word, words[word]))
+    
+  return tup_list
+
+print(find_most_common_words(obama_speech, 10))
+print(find_most_common_words(michelle_obama_speech, 10))
+print(find_most_common_words(donald_speech, 10))
+print(find_most_common_words(melina_trump_speech, 10))
